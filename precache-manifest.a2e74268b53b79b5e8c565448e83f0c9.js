daabeda5f152e65a7a17fc18ac8cfe36d2503e5f
Tahnee Hall <tnhall2012@hotmai.com>
2020-01-22 08:17:27 -0500

    Updates

---
+++ precache-manifest.a2e74268b53b79b5e8c565448e83f0c9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2464f41253fa5f8227146bff4867d240",
+    "url": "/memory/index.html"
+  },
+  {
+    "revision": "2500db64cd7967e8a4f5",
+    "url": "/memory/static/css/main.bbff8836.chunk.css"
+  },
+  {
+    "revision": "0fbf426dd97ed599a42f",
+    "url": "/memory/static/js/2.80953c26.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/memory/static/js/2.80953c26.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2500db64cd7967e8a4f5",
+    "url": "/memory/static/js/main.06c5f3ef.chunk.js"
+  },
+  {
+    "revision": "0b5d22b9f69562094125",
+    "url": "/memory/static/js/runtime-main.1bab3639.js"
+  }
+]);
